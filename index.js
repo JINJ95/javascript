71af10c4cb6dc326ee279cdd8ffc74f3b9c11803
Jordan Johnson <jordaninjohnson@gmail.com>
2021-10-31 22:43:42 -0600

    added second class & inheritance

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 // create new class
-class Person {
+class Human {
     constructor() {
+        this.gender = "male"
+    }
+
+    printGender() {
+        console.log(this.gender);
+    }
+}
+
+class Person extends Human{
+    constructor() {
+        // must call super constructor to use parent constructor
+        super();
         this.name = "Max"
+        // this would overwrite parent and make person female
+        //this.gender = 'female';
     }
 
     printMyName() {
@@ -11,3 +25,4 @@ class Person {
 
 const person = new Person();
 person.printMyName();
+person.printGender();
